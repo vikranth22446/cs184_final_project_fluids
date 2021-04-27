9bfa3f2d48011cfdbc7d6764d03c290ae1bd5dec
Kevin Lo <klo824@berkeley.edu>
2021-04-26 21:19:10 -0700

    Fixing vertex shader

--- src/src/index.js
+++ src/src/index.js
@@ -11,34 +11,34 @@ import throttle from "lodash/throttle"
 
 function w_poly(r, h) {
     if(r > h || r < 0 ) return 0;
-    return 315/(64*Math.PI * Math.pow(h, 9)) * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 3); 
+    return 315/(64*Math.PI * Math.pow(h, 9)) * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 3);
 }
 
 function w_poly_derivative6(r, h) {
     if(r > h || r < 0 ) return 0;
-    return 315/(64*Math.PI * Math.pow(h, 9)) * 3 * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 2) * (-2 * r); 
+    return 315/(64*Math.PI * Math.pow(h, 9)) * 3 * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 2) * (-2 * r);
 }
 function w_poly_laplacian(r, h) {
   if(r > h || r < 0 ) return 0;
-  return 315/(64*Math.PI * Math.pow(h, 9)) * 3 * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 2) * (-2) + 315/(64*Math.PI * Math.pow(h, 9)) * 6 * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 1) * (-2 * r); 
+  return 315/(64*Math.PI * Math.pow(h, 9)) * 3 * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 2) * (-2) + 315/(64*Math.PI * Math.pow(h, 9)) * 6 * Math.pow((Math.pow(h, 2) - Math.pow(r, 2)), 1) * (-2 * r);
 }
 
 function w_viscosity(r, h) {
-  if (r > h || r < 0) return 0; 
-  return 15/(2 * Math.PI * pow(h, 3)) * (-pow(r, 3)/(2 * pow(h, 3))) * (pow(r, 2)/pow(h,2)) * h/(2*r) - 1; 
+  if (r > h || r < 0) return 0;
+  return 15/(2 * Math.PI * pow(h, 3)) * (-pow(r, 3)/(2 * pow(h, 3))) * (pow(r, 2)/pow(h,2)) * h/(2*r) - 1;
 }
 
 function laplace_viscosity(r, h) {
-  if (r > h || r < 0) return 0; 
+  if (r > h || r < 0) return 0;
   return 45/(Math.PI * Math.pow(h, 6)) * (h-r)
 }
 
 function w_spiky(r, h) {
-    if (r > h || r < 0) return 0; 
+    if (r > h || r < 0) return 0;
     return 15/(Math.PI * pow(h, 6)) * pow((h - r), 3)
 }
 function w_spiky_derivative(r, h) {
-    if (r > h || r < 0) return 0; 
+    if (r > h || r < 0) return 0;
     return 15/(Math.PI * Math.pow(h, 6)) * 3 * Math.pow((h - r), 2) * (-1)
 }
 
@@ -49,7 +49,7 @@ function density_s(particle, particles) {
     for(let other_particle of particles) {
         const r = dist(other_particle.pos, particle.pos);
         p += other_particle.mass * w_poly(r, h);
-    }    
+    }
     particle.density = p;
     return p;
 }
@@ -58,7 +58,7 @@ function compute_pressure(particle, particles) {
     var f_p_x = 0.0;
     var f_p_y = 0.0;
     var f_p_z = 0.0;
-    const k = 1; // speed of sound 
+    const k = 1; // speed of sound
     const h = 1;
     const rho_zero = 1;
     const pi = k * (particle.density - rho_zero);
@@ -207,7 +207,7 @@ function Swarm({ count, ...props }) {
         const next_acc = vmuls(net_force, 1/particle_mass * delta_t * delta_t)
         var new_vel = vvadd(vel, vmuls(next_acc, delta_t))
         var new_pos = vvadd(pos, vmuls(new_vel, delta_t))
-        
+
         // if it hits the bounding box, comes back with opposite velocity
         if(new_pos.y < -viewport.height / 2 + 1) {
           new_pos.y = -viewport.height / 2 + 1
@@ -237,13 +237,13 @@ function Swarm({ count, ...props }) {
         particle.pos = new_pos
         particle.vel = new_vel
         // handle hitting the bounds
-       
+
 
         // temp = <x,y,z>
         dummy.position.set(new_pos.x, new_pos.y, new_pos.z);
         dummy.scale.set(1, 1, 1)
         dummy.updateMatrix()
-        // 
+        //
         // And apply the matrix to the instanced item
         mesh.current.setMatrixAt(i, dummy.matrix)
       })
@@ -308,6 +308,59 @@ void main() {
   )
 }
 
+fucntion material() {
+  const material = new THREE.ShaderMaterial({
+      // Choose values for these (copy-pasted from project 4 part 5)
+      /*
+        float k_a = 0.1;
+        float k_s = 0.5;
+        float p = 100.0;
+        float k_d = 1.0;
+        vec3 I_a = vec3(1.0, 1.0, 1.0);
+
+        vec3 l = normalize(u_light_pos - vec3(v_position));
+        vec3 n = normalize(vec3(v_normal));
+        vec3 v = normalize(u_cam_pos - vec3(v_position));
+        vec3 h = normalize(l + v);
+
+        vec3 diffuse = max(dot(n, l), 0.0) * k_d * normalize(u_light_intensity);
+        float angle = max(dot(h, n), 0.0);
+        vec3 spec = pow(angle, p) * k_s * normalize(u_light_intensity);
+
+        out_color = vec4(((k_a * I_a) + diffuse + spec) * vec3(u_color), 1.0);
+      */
+      // Reference: https://observablehq.com/@camargo/three-js-utah-teapot-with-a-custom-phong-shader-material
+      uniform_values: {
+          k_a: {value: 0.1},
+          k_d: {value: 1.0},
+          k_s: {value: 0.5},
+          I_a: {value: new THREE.Vector3(1.0, 1.0, 1.0)},
+          p: {value: 100.0}
+      },
+      vertex_shader: `
+        varying vec3 Normal;
+        varying vec3 Position;
+
+        void main() {
+          Normal = normalize(normalMatrix * normal);
+          Position = vec3(modelViewMatrix * vec4(position, 1.0));
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+      `,
+      fragment_shader: `
+        varying vec3 Normal;
+        varying vec3 Position;
+
+        uniform float k_a;
+        uniform float k_s;
+        uniform float p;
+        uniform float k_d;
+        uniform vec3 I_a;
+
+      `
+  });
+  return material;
+}
 
 function Stats() {
   const [stats] = useState(() => new StatsImpl())
@@ -368,7 +421,7 @@ function RotatingBox() {
         } else {
             myMesh.current.position.y -= 0.01
         }
-    });  
+    });
     const uniforms = {
       k_a: { value: new THREE.Vector3(0.9, 0.5, 0.3) },
       k_d: { value: new THREE.Vector3(0.9, 0.5, 0.3) },
@@ -403,12 +456,12 @@ function RotatingBox() {
     uniform vec4 LightPosition;
     uniform vec3 LightIntensity;
     vec3 bp_shading() {
-    vec3 n = normalize(Normal);
-    vec3 s = normalize(vec3(LightPosition) - Position);
-    vec3 v = normalize(vec3(-Position));
-    vec3 h = normalize(s) + normalize(v);
-      return LightIntensity * (k_a + k_s * pow(max(dot(h, n),0.0), p) + k_d * max(dot(s, Normal), 0.0 ));
-  }
+      vec3 n = normalize(Normal);
+      vec3 s = normalize(vec3(LightPosition) - Position);
+      vec3 v = normalize(vec3(-Position));
+      vec3 h = normalize(s) + normalize(v);
+      return LightIntensity * (k_a + k_s * pow(max(dot(h, n),0.0), p) + k_d * max(dot(s, n), 0.0 ));
+    }
   void main() {
     gl_FragColor = vec4(bp_shading(), 1.0);
   }
@@ -438,13 +491,14 @@ function App() {
         <ambientLight intensity={0.5} />
 
         <directionalLight/>
+        <ambientLight/>
         {/* <Swarm count={5} position={[0, 10, 0]} /> */}
         {/* <ContactShadows rotation={[Math.PI / 2, 0, 0]} position={[0, -30, 0]} opacity={0.4} width={100} height={100} blur={1} far={40} /> */}
         {/* <EffectComposer multisampling={0}>
           <SSAO samples={31} radius={10} intensity={20} luminanceInfluence={0.1} color="red" />
         </EffectComposer> */}
         {/* <Suspense fallback={null}>
-          <Environment preset="city" /> 
+          <Environment preset="city" />
         </Suspense>  */}
         <Physics>
           <Borders></Borders>
