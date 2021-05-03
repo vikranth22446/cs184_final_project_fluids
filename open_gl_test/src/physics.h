507f9bdbe3b8a44c9191de3e5f2ea4a0f3ac0e15
vikranth22446 <rama22446@gmail.com>
2021-05-03 00:59:03 -0700

    Add gaurds to header file to prevent reimporting twice

--- open_gl_test/src/physics.h
+++ open_gl_test/src/physics.h
@@ -1,3 +1,6 @@
+#ifndef FLUID_PHYSICS_H
+#define FLUID_PHYSICS_H
+#include "particle.h"
 #include "glm/glm.hpp"
 
 float dist(glm::vec3 pos1, glm::vec3 pos2); 
@@ -8,8 +11,10 @@ float w_viscosity(float r, float h);
 float laplace_viscosity(float r, float h); 
 float w_spiky(float r, float h); 
 float w_spiky_derivative(float r, float h); 
-float density_s(Particle particle, Particle[] particlesContainer); 
-glm::vec3 compute_pressure(Particle particle, Particle[] particlesContainer); 
-glm::vec3 compute_viscosity(Particle particle, Particle[] particlesContainer); 
-glm::vec3 compute_gravity(Particle particle); 
-glm::vec3 compute_surface_tension(Particle particle, Particle[] particlesContainer); 
+float density_s(Particle *particle, int max_particles, Particle particlesContainer[]);
+glm::vec3 compute_pressure(Particle *particle, int max_particles, Particle particlesContainer[]);
+glm::vec3 compute_viscosity(Particle *particle, int max_particles, Particle particlesContainer[]);
+glm::vec3 compute_gravity(Particle *particle, int max_particles, Particle particlesContainer[]);
+glm::vec3 compute_surface_tension(Particle *particle, int max_particles, Particle particlesContainer[]);
+
+#endif
