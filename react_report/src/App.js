97ba208746f400cd5a6d1d630927e6ba8bfe2886
vikranth22446 <rama22446@gmail.com>
2021-05-07 15:11:49 -0700

    Add details about Abstract, Motivations, and Current Framework

--- react_report/src/App.js
+++ react_report/src/App.js
@@ -11,16 +11,34 @@ function App() {
   return (
     <div>
   <Navbar bg="dark" variant="dark" class="shadow p-3 mb-5 bg-body rounded">
-    <Navbar.Brand>3D Interactive Fluid Simulation Report</Navbar.Brand>
-  
+    <Navbar.Brand><h2>3D Interactive Fluid Simulation Report</h2></Navbar.Brand>
+
   </Navbar>
+    <Navbar.Brand><h4>Vikranth Srivatsa, Bhuvan Basireddy, Kevin Lo, Benjamin Cheung</h4></Navbar.Brand>
   <br />
   <Container>
     <Row><h2>Abstract</h2></Row>
-    <Row><p>...</p></Row>
+    <Row><p>Fluid simulations such as those with water, fire, smoke are very important in realtime games and visual effects. 
+      The many realistic water mods from games such as Skyrim and Minecraft provide a strong case study of their usability. Particle simulation among other approaches is popular method of simulating these effects.
+      The study of these simulations particle simulations is usually based around Navier-Stokes fluid dynamics equations. Smoothed Particle Simulation is a method to extend these equations. 
+      The paper Particle-Based FluidSimulation for Interactive Applications presents an approach to approximate these interactions. 
+      We extend and attempt to replicate this project, while adding providing an interactive realtime demo for others to extend.    
+      </p></Row>
+    <Row><h2>Motivations</h2></Row>
+    <Row><p>There were previous semesters of CS184 that presented fluid simulation. These fluid simulations were usually 2D, which greatly reduced the complexity, and pre-rendered for their demonstration. 
+      However, for popular games and visual effects, these environment is 3D and close to realtime and not pre-rendered. This greatly increases the complexity and computational requirements to render. 
+      The project is our attempt at creating this realtime effect with a large number of 3D particles with user interactions.</p>
+    </Row>
     <Row><h2>Technical approach</h2></Row>
+    <Row><p>The project was implemented C++ and OpenGL for speed and performance.
+       The sections below describe the technical approaches and advantages of our approaches</p></Row>
     <Row><h5>Initial Framework: Three.js</h5></Row>
     <Row><h5>Current Framework: C++ and OpenGL</h5></Row>
+    <Row><p>
+      After implementing in three.js, we wanted something that would perform well. 
+      We transitioned to implementing the algorthim and logic from scratch in C++ and OpenGL. 
+      
+      </p></Row>
     <Row><h5>Rendering Loop and Logic</h5></Row>
     Logic about VBO + creating Spheres
     <Row><h5>Simulation and Physics Logic</h5></Row>
